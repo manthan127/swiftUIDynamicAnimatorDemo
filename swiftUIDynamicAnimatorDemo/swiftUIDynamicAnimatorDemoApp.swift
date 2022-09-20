15ad1f79b1594f8251ca72442dec47a6ce9a913a
Manthan <107455609+manthan127@users.noreply.github.com>
2022-09-20 15:00:22 +0530

    Initial Commit

---
+++ swiftUIDynamicAnimatorDemo/swiftUIDynamicAnimatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIDynamicAnimatorDemoApp.swift
+//  swiftUIDynamicAnimatorDemo
+//
+//  Created by mac on 20/09/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIDynamicAnimatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
