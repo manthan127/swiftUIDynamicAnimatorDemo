85dd71bf7db5eb28909c6b859f366e44a9db9eea
Manthan <107455609+manthan127@users.noreply.github.com>
2022-09-27 11:38:07 +0530

    page controller complete

--- swiftUIDynamicAnimatorDemo/AnimatorViewRepresentable.swift
+++ swiftUIDynamicAnimatorDemo/AnimatorViewRepresentable.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct AnimatorViewRepresentable: UIViewRepresentable {
     let view = UIView()
-    let p = MyTestViewController()
+    let p = MyPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
     static var coordinator: Coordinator!
     @Binding var playing: Bool
     
@@ -195,12 +195,10 @@ struct AnimatorViewRepresentable: UIViewRepresentable {
 }
 
 class ExampleViewController: UIViewController {
-
-    let theLabel: UILabel = {
-        let v = UILabel()
+    let theLabel: UIImageView = {
+        let v = UIImageView()
         v.translatesAutoresizingMaskIntoConstraints = false
         v.backgroundColor = .white
-        v.textAlignment = .center
         return v
     }()
 
@@ -209,61 +207,61 @@ class ExampleViewController: UIViewController {
 
         view.addSubview(theLabel)
         NSLayoutConstraint.activate([
-            theLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            theLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            theLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            theLabel.topAnchor.constraint(equalTo: view.topAnchor),
+            theLabel.widthAnchor.constraint(equalTo: view.widthAnchor),
+            theLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 10)
         ])
     }
 }
 
-let colors: [UIColor] = [
-    .red,
-    .green,
-    .blue,
-    .cyan,
-    .yellow,
-    .orange
-]
 class MyPageViewController: UIPageViewController {
-    var pages: [UIViewController] = [UIViewController]()
-
-    override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
-        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-
+    let colors: [UIColor] = [
+        .red,
+        .green,
+        .blue,
+        .cyan,
+        .yellow,
+        .orange
+    ]
+    
+    lazy var pages = ["trash", "folder.fill", "folder.badge.person.crop", "mic"]
+        .compactMap{ s -> UIViewController in
+            let x = ExampleViewController()
+            x.theLabel.image = UIImage(systemName: s)
+            x.theLabel.backgroundColor = colors.randomElement() ?? .red
+            return x
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         dataSource = self
         delegate = nil
 
-        // instantiate "pages"
-        for i in 0..<colors.count {
-            let vc = ExampleViewController()
-            vc.theLabel.text = "Page: \(i)"
-            vc.view.backgroundColor = colors[i]
-            pages.append(vc)
-        }
-
+        view.backgroundColor = .gray
         setViewControllers([pages[0]], direction: .forward, animated: false, completion: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        for view in self.view.subviews {
+            if let _ = view as? UIScrollView {
+                view.frame = self.view.frame
+            } else if let _ = view as? UIPageControl {
+                view.backgroundColor = .clear
+            }
+        }
     }
 }
 
-// typical Page View Controller Data Source
 extension MyPageViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-
         guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
 
         let previousIndex = viewControllerIndex - 1
-
         guard previousIndex >= 0 else { return pages.last }
 
-        guard pages.count > previousIndex else { return nil }
+//        guard pages.count > previousIndex else { return nil }
 
         return pages[previousIndex]
     }
@@ -272,77 +270,20 @@ extension MyPageViewController: UIPageViewControllerDataSource {
         guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
 
         let nextIndex = viewControllerIndex + 1
-
         guard nextIndex < pages.count else { return pages.first }
 
-        guard pages.count > nextIndex else { return nil }
+//        guard pages.count > nextIndex else { return nil }
 
         return pages[nextIndex]
     }
 }
 
-// typical Page View Controller Delegate
 extension MyPageViewController: UIPageViewControllerDelegate {
-
-    // if you do NOT want the built-in PageControl (the "dots"), comment-out these funcs
-
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
         return pages.count
     }
 
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-
-        guard let firstVC = pageViewController.viewControllers?.first else {
-            return 0
-        }
-        guard let firstVCIndex = pages.firstIndex(of: firstVC) else {
-            return 0
-        }
-
-        return firstVCIndex
-    }
-}
-//
-class MyTestViewController: UIViewController {
-
-    let myContainerView: UIView = {
-        let v = UIView()
-        v.translatesAutoresizingMaskIntoConstraints = false
-        v.backgroundColor = .gray
-        return v
-    }()
-
-    var thePageVC: MyPageViewController!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // add myContainerView
-        view.addSubview(myContainerView)
-        
-        NSLayoutConstraint.activate([
-            myContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            myContainerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            myContainerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            myContainerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-
-        // instantiate MyPageViewController and add it as a Child View Controller
-        thePageVC = MyPageViewController()
-        addChild(thePageVC)
-
-        // we need to re-size the page view controller's view to fit our container view
-        thePageVC.view.translatesAutoresizingMaskIntoConstraints = false
-//        UIStackView(arrangedSubviews: [thePageVC.view])
-        // add the page VC's view to our container view
-
-        myContainerView.addSubview(thePageVC.view)
-        thePageVC.didMove(toParent: self)
-        NSLayoutConstraint.activate([
-            thePageVC.view.topAnchor.constraint(equalTo: myContainerView.topAnchor, constant: 0.0),
-            thePageVC.view.bottomAnchor.constraint(equalTo: myContainerView.bottomAnchor, constant: 0.0),
-            thePageVC.view.leadingAnchor.constraint(equalTo: myContainerView.leadingAnchor, constant: 0.0),
-            thePageVC.view.trailingAnchor.constraint(equalTo: myContainerView.trailingAnchor, constant: 0.0),
-        ])
+        return 0
     }
 }
